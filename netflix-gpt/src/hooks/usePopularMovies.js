b0c619a977370c9f55d409683ac88be2b618e830
Nitesh <iamnitesh22@gmail.com>
2024-05-27 02:42:27 +0530

    Secondary Container with movie List && usePopular Movies

---
+++ netflix-gpt/src/hooks/usePopularMovies.js
@@ -0,0 +1,23 @@
+import { API_OPTIONS } from "../Constants";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addPopularMovies } from "../utils/movieSlice";
+const usePopularMovies = () => {
+  const dispatch = useDispatch();
+
+  const getPopularMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    dispatch(addPopularMovies(json.results));
+    // console.log(json.results);
+  };
+
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+};
+
+export default usePopularMovies;
